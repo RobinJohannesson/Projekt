42e885ed1e5010cb907993bd045db3f5314bdaf7
emmaajohaansson <emmakristina95@hotmail.com>
2016-05-24 16:31:36 +0200

    bakgrunder

--- show_travel.php
+++ show_travel.php
@@ -67,20 +67,6 @@ if(isset($_GET['id'])) {
                 <li>&gt;&gt;Städer</li>
             </ol>
         </div>
-        <div class="dropdown">
-            <div class="dropbtn">
-                <img src="img/arrow_dropdown.png" class="arrow_for_dropdown">
-                <div class="dropdown_name">
-                    <p>Wade</p>
-                    <p>Wilson</p>
-                </div>
-                <img src="img/deadpool_avatar.png" class="avatar_img">
-            </div>
-            <div class="dropdown_content">
-                <a href="#">Inställningar</a>
-                <a href="#">Logga ut</a>
-            </div>
-        </div>
     </header>
     <div id="wrapper">
         <?php
